3075ba6b910e01e057932aef37ec9dab047db2e2
sven-ervin.paap <sven-ervin.paap@ut.ee>
2022-12-03 23:51:47 +0200

    1st Day1

--- 01/Day1.py
+++ 01/Day1.py
@@ -1,24 +1,3 @@
-def solverProblem1(measurements):
-    increase = 0
-
-    for i in range(1, len(measurements)):
-        if measurements[i] > measurements[i - 1]:
-            increase += 1
-
-    print("Problem 1 answer: " + increase)
-
-
-def solverProblem2(measurements):
-    increase = 0
-
-    for i in range(2, len(measurements) - 1):
-        if (measurements[i - 2] + measurements[i - 1] + measurements[i]) < (
-                measurements[i - 1] + measurements[i] + measurements[i + 1]):
-            increase += 1
-
-    print("Problem 2 answer: " + increase)
-
-
 if __name__ == '__main__':
     with open("01\input.txt") as file:
         elfs = [s.split("\n") for s in file.read().split("\n\n")]
